b74ba2b1d37325c731204b019c6a38031286302c
Huzaifa-Faizee <96124697+Huzaifa-Faizee@users.noreply.github.com>
2023-10-13 12:45:27 +0530

    skills section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/navbar";
 import HeroSection from "./components/HeroSection";
+import ImporveSkills from "./components/improveSkill";
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <div className="container main">
         <HeroSection/>
+        <ImporveSkills/>
       </div>
     </div>
   );
