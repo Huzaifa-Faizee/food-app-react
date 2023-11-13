1689e7fea609a49f609948788d995fe27c9d32dd
Huzaifa-Faizee <96124697+Huzaifa-Faizee@users.noreply.github.com>
2023-11-13 18:48:12 +0530

    Quote Section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar";
 import HeroSection from "./components/HeroSection";
 import ImporveSkills from "./components/improveSkill";
+import QuoteSection from "./components/QuoteSection";
 function App() {
   return (
     <div className="App">
@@ -8,6 +9,7 @@ function App() {
       <div className="container main">
         <HeroSection/>
         <ImporveSkills/>
+        <QuoteSection />
       </div>
     </div>
   );
